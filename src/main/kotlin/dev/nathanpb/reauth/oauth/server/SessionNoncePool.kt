ea16e27d8e529d288d9cf92d3e87cb916cf6972f
NathanPB <nathan.pbombana@gmail.com>
2021-03-07 19:01:21 -0300

    I fucked up the license

--- src/main/kotlin/dev/nathanpb/reauth/oauth/server/SessionNoncePool.kt
+++ src/main/kotlin/dev/nathanpb/reauth/oauth/server/SessionNoncePool.kt
@@ -3,18 +3,18 @@
  *
  * This file is part of Reauth.
  *
- * Wheres My Duo is free software: you can redistribute it and/or modify
+ * ReAuth is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Wheres My Duo is distributed in the hope that it will be useful,
+ * ReAuth is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Wheres My Duo.  If not, see <https://www.gnu.org/licenses/>.
+ * along with ReAuth.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package dev.nathanpb.reauth.oauth.server
